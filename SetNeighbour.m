8639d099e54cf2c33602400733918729e75f09bb
zilongzhong <zhongzl924@gmail.com>
2016-08-29 23:26:17 -0400

    Using sparse ()

--- SetNeighbour.m
+++ SetNeighbour.m
@@ -1,6 +1,6 @@
 function CMatrix = SetNeighbour(Row, Colume)
-NumSites = int32(Row*Colume);
-CMatrix = zeros(NumSites, NumSites);
+NumSites = Row*Colume;
+CMatrix = sparse(NumSites, NumSites);
 for i=1:NumSites
     if i+1 <= NumSites && mod(i,Row)~=0
         CMatrix(i,i+1) = 1;
